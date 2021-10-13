75bd5abc7302bedae4b053ffcfe748e59c257072
Victor Moraes <victor@vulpi-ideas.com>
2021-10-13 18:48:53 -0300

    improved installation by using get_env and set_env

--- sql/01-products.sql
+++ sql/01-products.sql
@@ -1,10 +1,11 @@
 CREATE TABLE products (
-    prod_id BIGSERIAL NOT NULL PRIMARY KEY,
-    prod_name TEXT NOT NULL,
-    prod_descr TEXT,
-    prod_image BYTEA,
-    prod_price BIGINT NOT NULL,
-    prod_instock BIGINT NOT NULL,
+    prod_id         BIGSERIAL NOT NULL PRIMARY KEY,
+    prod_name       TEXT NOT NULL,
+    prod_descr      TEXT,
+    prod_img        BIGINT
+                    REFERENCES pics(pic_id) ON DELETE CASCADE,
+    prod_price      BIGINT NOT NULL,
+    prod_instock    BIGINT NOT NULL,
 
     CHECK (CHAR_LENGTH(prod_name) > 0),
     CHECK (prod_price > 0),
